c2f9d09504e4ec4134c499eba540c2e8f76c0759
annapurna8050 <68692570+annapurna8050@users.noreply.github.com>
2020-08-11 20:39:41 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
+function displayTypedText()
+{	
+	if(document.getElementById("putText").value.length==0)
+	{
+		var s="please enter something";
+	
+		document.getElementById("ifEmpty").innerHTML=s;
+		
+		console.log(s);
 
+	}
+
+
+	else
+	{
+		
+	document.getElementById("getText").innerHTML=document.getElementById("putText").value;
+	
+	document.getElementById("putText").value="";
+	document.getElementById("ifEmpty").innerHTML="";
+	}
+}
